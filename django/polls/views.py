610b0ddbaaf1079284423e674fa1f4320db0eb1e
himanmen Git <study.himanmen@gmail.com>
2018-02-02 15:28:59 +0900

    detail view에 Choice목록을 출력

--- django/polls/views.py
+++ django/polls/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from django.template import loader
 
-# Create your views here.
 from polls.models import Question
 
 
@@ -13,20 +13,43 @@ def index(request):
         'latest_question_list': latest_question_list
     }
 
-    # 쉼표단위로 구분된 Question목록의 각 항목의 question_text로 만들어진 문자열
+    # render를 사용
+    return render(request, 'polls/index.html', context)
+
+    # template을 명시적으로 불러와 rendering
+    # template = loader.get_template('polls/index.html')
+    # return HttpResponse(template.render(context, request))
+
+    # 쉼표단위로 구분된 Question목록의 각 항목의 question_text로 만들어진 문자열을 리턴
     # output = ', '.join([q.question_text for q in latest_question_list])
     # return HttpResponse(output)
 
-    return render(request, 'polls/index.html', context)
-
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    """
+    question_id에 해당하는 Question객체를 템플릿에 전달
+        key: "question"
+        template: polls/templates/polls/detail.html
+    템플릿 에서는
+    1. 전달 받은 question의 question_text를 출력
+        {{ question,question_text }}
+    2. question이 가진 모든 Choice들을 ul > li로 출력
+        {{ {% for choice in question.choice_set.all %}
+
+    :param request:
+    :param question_id:
+    :return:
+    """
+    question = Question.objects.get(pk=question_id)
+    context = {
+        "question": question,
+    }
+    return render(request, 'polls/detail.html', context)
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+
+    return HttpResponse("You're results on question %s." % question_id)
 
 
 def vote(request, question_id):
